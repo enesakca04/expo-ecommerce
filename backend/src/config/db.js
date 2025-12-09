827dc0b03db7365a86d7f85018cff44d33d5f7ee
enes akca <eneesakcaa@gmail.com>
2025-12-09 23:34:53 +0300

    startServer method added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,7 @@ export const connectDB = async()=>{
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log('Connected to MONGODB: ${conn.connection.host}');
     } catch(error){
-        console.error("💥MONGODB connection error");
+        console.error("MONGODB connection error");
         process.exit(1); // exit code 1 means failure, 0 means success
         
     }
